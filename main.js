960f7f60707b5923c3ad72e543fb1ec7c416fb37
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-07-22 20:23:12 -0700

    Added moving obsticles.

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 const car = document.querySelector('.img1');
-
+const dots1 = document.querySelector('.dots1');
+const dots2 = document.querySelector('.dots2');
+let rotatingObsticles = setInterval(handleObsticles, 30);
 document.addEventListener('keydown', handleKeyDown);
 
-function handleKeyDown(event) {
+function handleObsticles() {
+  dot1Number += 10;
+  dot2Number -= 10;
+  if (dot1Number >= 90 && dot1Number <= 410) {
+    dots1.style.top = `${dot1Number}px`;
+    } else {
+      dot1Number2 -= 10;
+      dots1.style.top = `${dot1Number2}px`;
+      if (dot1Number2 === 90) {
+        dot1Number = 90;
+        dot1Number2 = 410;
+      }
+    }
+  if (dot2Number <= 850 && dot2Number >= 560) {
+    dots2.style.top = `${dot2Number}px`;
+  } else {
+    dot2Number2 += 10;
+    dots2.style.top = `${dot2Number2}px`;
+    if (dot2Number2 === 850) {
+      dot2Number = 850;
+      dot2Number2 = 560;
+    }
+  }
+  }
 
+function handleKeyDown(event) {
   let keyPressed = event.which;
   const keyboardButton = { left: 37, up: 38, right: 39, down: 40, space: 32};
   if (keyPressed === keyboardButton.left) {
@@ -47,5 +73,32 @@ function handleSetInterval() {
   if (carDirection === 'right') {
     acceleration.left += 10;
     car.style.left = `${acceleration.left}px`;
+  } else if (carDirection === 'down') {
+    acceleration.top += 10;
+    car.style.top = `${acceleration.top}px`;
+  } else if (carDirection === 'left') {
+    acceleration.left -= 10;
+    car.style.left = `${acceleration.left}px`;
+  } else if (carDirection === 'up') {
+    acceleration.top -= 10;
+    car.style.top = `${acceleration.top}px`;
+  }
+
+  if (acceleration.left === 1900) {
+    acceleration.left = -200;
+  } else if (acceleration.left === -200) {
+    acceleration.left = 1900;
+  } else if (acceleration.top === -200) {
+    acceleration.top = 900;
+  } else if (acceleration.top === 1000) {
+    acceleration.top = -200;
+  }
+  // stationary dots
+  // if (acceleration.left >= 810 && acceleration.top === 570) {
+  //   console.log('You loose!');
+  // }
+  // moving dots
+  if (acceleration.left === 1000 && acceleration.top === dot1Number) {
+    console.log('You loose!');
   }
 }
